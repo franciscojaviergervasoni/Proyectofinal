e0e9eaa48c643811bf9713458b19d495af8ff414
francisco gervasoni <franciscojg_15@hotmail.com>
2023-02-09 15:51:45 -0300

    first commit

---
+++ argentinaprograma/proyectofinal/proyectofinal/CV/javaCV (2).js
@@ -0,0 +1,30 @@
+
+/**
+document.getElementBy("click",function () {
+    document.IdaddEventListener("demo").style.visibility
+    
+})
+     
+document.getElementById("boton").onclick = function aparecer(){}
+function aparecer() {document.getElementById("demo").style="display:contents;"
+    
+    para alerta
+
+    document.getElementsByName(Emviar) = function avisar() {
+            alert("Formulario enviado pronto te contactare" )
+            
+        }
+        
+}
+    
+}
+
+
+document.getElementById("boton").onclick = function aparecer() {
+    document.getElementById("demo").style="display:contents;"}*/
+    document.getElementById("boton").onclick = function aparecer() {
+        document.getElementById("demo").style="display:block;"
+    document.getElementById("barranav").style="display:block;"}
+    
+        
+    
